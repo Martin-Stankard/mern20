e40c87d5495e5074d21823358438c8d6adec8c68
Martin <martin.stankard@hotmail.com>
2020-12-10 18:25:29 -0700

    h1 post, posts and form stubs working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-import memories from './images/memories.png'
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
 const App =() => {
     return(
         <Container maxWidth='lg'>
