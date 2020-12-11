62e56977c266af40c5fc78488ac09794ca48becc
Martin <martin.stankard@hotmail.com>
2020-12-10 17:20:28 -0700

    abstracted out controller function

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js'
 
 const router =  express.Router();
 
 //localhost:5000/posts | icky, /posts lives in caller
-router.get('/', (req,res)=>{
-    res.send('This Works!!!');
-});
+router.get('/', getPosts);
 
 export default router;
