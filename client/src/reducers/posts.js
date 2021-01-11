0bf8a84c233816f13da6f9a95104276431f9b3b7
Martin <martin.stankard@hotmail.com>
2021-01-11 16:16:00 -0700

    caught style typos; onto db test

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,15 +7,13 @@ const posts = (posts=[], action) => {
     switch(action.type){
         case 'FETCH_ALL':
             return action.payload;
-            break;
-
+            
         case 'CREATE':
             return [ ...posts , action.payload ];
-            break;
-
+            
         default:
             return posts;
-            break;
+            
     }
 }
 
