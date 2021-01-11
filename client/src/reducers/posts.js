1729735f340218c1aa716f16f7a98d2bebe63903
Martin <martin.stankard@hotmail.com>
2021-01-11 07:38:06 -0700

    redux provider in place

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,22 @@
+// simplified notes
+//const reducer = (state, action) => {
+    // if(action.type==='CREATE'){
+    //     return state modified by action
+    // }
+const posts = (posts=[], action) => {
+    switch(action.type){
+        case 'FETCH_ALL':
+            return posts;
+            break;
+
+        case 'CREATE':
+            return posts;
+            break;
+
+        default:
+            return posts;
+            break;
+    }
+}
+
+export default posts;
