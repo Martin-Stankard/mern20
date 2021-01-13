2625fe4afb866025e1ffeccab70970a3f0962fa4
Martin <martin.stankard@hotmail.com>
2021-01-13 07:51:33 -0700

    never underestimate the difference between () and {}

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -10,14 +10,14 @@ const Posts = () => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
-    return(
+    return (
         !posts.length ? <CircularProgress/> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map( (post) => {
+                {posts.map( (post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
                         <Post post={post}/>
                     </Grid>
-                })}
+                ))}
             </Grid>
         )
     );
