405acb3cc92a0d2a9f2d5a7301b70b6ec5cf67cc
Martin <martin.stankard@hotmail.com>
2021-01-13 05:44:17 -0700

    debugging mongo connection. collection is created but document is not...

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -7,10 +7,8 @@ import useStyles from "./styles.js";
 
 
 const Posts = () => {
-    const posts = useSelector((state)=>state.posts);
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
-
-    console.log(posts);
 
     return(
         !posts.length ? <CircularProgress/> : (
