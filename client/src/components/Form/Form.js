9b16a642640410dcd1111c8436d54eccef29e47a
Martin <martin.stankard@hotmail.com>
2021-01-13 18:57:18 -0700

    onto debugging the update functionality, not populating form....

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from "./styles.js";
 
 import {createPost, updatePost} from '../../actions/posts';
 
-//TODO get the current id of post selected by user in Post component
+import {  } from 'react-redux';
+
 
 const Form = ({currentId, setCurrentId}) => {
     const [postData, setPostData] = useState({
@@ -18,8 +18,15 @@ const Form = ({currentId, setCurrentId}) => {
         tags: '',
         selectedFile: ''
     });
+
+    const post = useSelector((state) => currentId ? state.posts.find((p)=> p._id === currentId) : null);
+
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if(post) setPostData(post);
+    }, [post]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
