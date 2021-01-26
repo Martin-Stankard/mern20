7aec03e29d3d7376764772af95318a4c89843cee
Martin <martin.stankard@hotmail.com>
2021-01-26 07:58:22 -0700

    git surgery verified

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/posts", postRoutes);
 
 // heroku lkes PORT
 const PORT = process.env.PORT || 5000;
-
+//git surgery verified. This code was insecure in a previous life
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
